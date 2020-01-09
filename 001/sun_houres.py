2a015021fa04aaeae5f151508fcbe8a25eae54d0
Eryk Hammer <erykhammer@wp.pl>
2020-01-09 01:25:01 +0100

    split program into smaller ones

---
+++ 001/sun_houres.py
@@ -0,0 +1,14 @@
+def sunrise_sunset():
+    e = api.load('de421.bsp')
+    ts = api.load.timescale()
+    bluffton = api.Topos('65.02 N', '25.47 E')  # Oulu coordinates
+    t0 = ts.utc(2019, 3, 1, 0)
+    t1 = ts.utc(2019, 4, 1, 0)
+    t, y = almanac.find_discrete(t0, t1, almanac.sunrise_sunset(e, bluffton))
+
+    sunrises = np.array(t.utc_iso()[::2],
+                        dtype=pd._libs.tslibs.timestamps.Timestamp)  # gets every element which index is even
+    sunsets = np.array(t.utc_iso()[1::2],
+                       dtype=pd._libs.tslibs.timestamps.Timestamp)  # gets every element which index is odd
+    # sunrises = sunrises.replace(tzinfo=None)
+    return sunrises, sunsets
